c8596e4e1b7c5a991ffd71de53664d69f9065c8f
gerunchan <grcamazing@163.com>
2021-04-15 12:39:04 +0800

    null

--- mc/src/main/java/com/matcloud/mc/config/MyWebConfigurer.java
+++ mc/src/main/java/com/matcloud/mc/config/MyWebConfigurer.java
@@ -3,10 +3,6 @@ package com.matcloud.mc.config;
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.web.servlet.config.annotation.*;
 
-/**
- * @author Evan
- * @date 2019/4
- */
 @SpringBootConfiguration
 public class MyWebConfigurer implements WebMvcConfigurer {
 
--- mc/src/main/java/com/matcloud/mc/config/ShiroConfiguration.java
+++ mc/src/main/java/com/matcloud/mc/config/ShiroConfiguration.java
@@ -18,10 +18,6 @@ import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
-/**
- * @author Evan
- * @date 2019/4
- */
 @Configuration
 public class ShiroConfiguration {
     @Bean
